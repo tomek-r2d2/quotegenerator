a1ca8735113fa0bc8cc69adb6bf3b78f351b9e4d
tomek-r2d2 <tomek.r2d2@gmail.com>
2017-06-12 13:52:20 +0200

    DZIALAJACY GEENRATOR

--- src/index.js
+++ src/index.js
@@ -4,47 +4,65 @@ import './index.css';
 
 // ========================================
 // https://facebook.github.io/react/docs/handling-events.html
-class GenerateButton extends React.Component {
 
-  renderQuote() {
+class Quotes {
 
-    return (
-      <div>
-        gothca
-      </div>
+  constructor() {
+    console.log("sdfdsfds");
 
-    );
+    this.quotes = ["Zamknij się!",
+      "Tomek, czy Tobie też staje (na signer-ze)?",
+      "Jeśli Content-Transfer-Encoding było base64 to dataHandler part.getContent zwraca"];
+    this.lastQuote = null;
   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={() => this.renderQuote()}>
-          Wygeneruje Rdatowy cytat
-      </button>
-      </div>
-    );
-  }
+  generateQuote() {
 
+    console.log("generuje !!");
+    let tmp = -1;
+
+    do {
+      tmp = Math.floor((Math.random() * this.quotes.length) + 1);
+       }
+    while (tmp === this.lastQuote)
+
+    console.log(this.quotes[tmp]);
+
+    this.lastQuote = tmp;
+    return this.quotes[tmp];
+  }
 }
 
 
 class App extends React.Component {
 
-  render() {
-    return (
 
-      <div>
+  constructor(props) {
+    super(props);
 
-        <GenerateButton />
-      </div>
+    this.state = {
+      data: 'Wygeneruj cytat',
+      quote: new Quotes()
+    }
 
+    this.updateState = this.updateState.bind(this);
 
+  };
 
-    );
+  updateState() {
+    console.log("robie update");
 
+    this.setState({ data: this.state.quote.generateQuote() })
   }
 
+  render() {
+    return (
+      <div>
+        <button onClick={this.updateState}>Wygeneruj cytat</button>
+        <h4>{this.state.data}</h4>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
